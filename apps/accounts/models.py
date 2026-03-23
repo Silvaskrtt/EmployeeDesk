2ae9da809e8d0f86c98c6be1e6a968aaec10b1a2
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-03-23 11:18:48 -0300

    config: configuração parcial do user.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,4 +2,13 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import Group
+import os
 
+def avatar_upload_path(instance, filename):
+    """Gera caminho único para o avatar"""
+    ext = filename.split('.')[-1]
+    filename = f"{instance.user.username}_avatar.{ext}"
+    return os.path.join('avatars', filename)
